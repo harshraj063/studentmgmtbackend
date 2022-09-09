8b09dbf5998b76d75c688e76e6428215a39bbe16
DEVCHANGELA12 <20it014@charusat.edu.in>
2022-09-09 12:21:40 +0530

    GFirst commit

---
+++ src/main/java/com/example/studenrmgmtbackend/StudenrmgmtbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.studenrmgmtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenrmgmtbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudenrmgmtbackendApplication.class, args);
+	}
+
+}
